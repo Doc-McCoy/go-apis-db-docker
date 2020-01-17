3b8f76729002a99842ae25a4e9516e8e240458d7
Renan <renan_ota@hotmail.com>
2020-01-17 19:28:48 -0300

    Commit inicial

---
+++ db/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE contato (
+    id serial,
+    nome VARCHAR(40) NOT NULL,
+    numero = VARCHAR(11) NOT NULL,
+    valor_aleatorio INT,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO contato (nome, numero) VALUES
+('Vanessa da mata', '19992345456'),
+('Negretude Júnior', '19992345678'),
+('Jack Bauer', '19998785675');
